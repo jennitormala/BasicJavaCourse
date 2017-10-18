18462a9597d1a983cd4ca96f12180afe3f36d9ca
zxander94 <alexandurpetrov94@abv.bg>
2017-10-18 14:51:18 +0300

    Task 3 Completed

--- src/com/omisoft/aatanasov/javacourse/introinjava/task3/Test.java
+++ src/com/omisoft/aatanasov/javacourse/introinjava/task3/Test.java
@@ -4,8 +4,6 @@ public class Test {
     public static void main(String[] args) {
         WeightOfArray weight = new WeightOfArray();
         System.out.println("[1,2,3,4,5,6,7,8,9]: " + weight.findWeight(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
-
-
     }
 }
 
